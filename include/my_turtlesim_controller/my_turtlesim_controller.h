cb2d88539118c323007e39455366d3f4c364a764
ReiFujise00 <rei.fujise@techouse.jp>
2023-02-13 17:33:30 +0900

    first commit

---
+++ include/my_turtlesim_controller/my_turtlesim_controller.h
@@ -0,0 +1,30 @@
+#ifndef MY_TURTLESIM_CONTROL_H
+#define MY_TURTLESIM_CONTROL_H
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <turtlesim/Pose.h>
+
+class MyTurtlesimController {
+    public:
+        MyTurtlesimController();
+        void process();
+
+    private:
+        void pose_callback(const turtlesim::Pose::ConstPtr &msg);
+
+        void straight();
+        void turn();
+
+        int hz_;
+
+        ros::NodeHandle nh_;
+        ros::NodeHandle private_nh_;
+        ros::Publisher pub_cmd_vel_;
+        ros::Subscriber sub_pose_;
+
+        turtlesim::Pose current_pose_;
+        geometry_msgs::Twist cmd_vel_;
+};
+
+#endif
